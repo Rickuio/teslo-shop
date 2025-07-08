655edcf954dd1bfabb07224a511d0b3cb0eddcb4
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2025-07-08 10:07:24 -0500

    Subiendo el backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'front',
+        path: '',
         loadChildren: () => import('./store-front/store-front.routes'),
     },
     {
