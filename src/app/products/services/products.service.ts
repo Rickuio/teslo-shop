ca4ff74894be9b933730f77edade3598077626ae
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2025-07-10 10:36:38 -0500

    Primer consumo de productos

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProductsResponse } from '@products/interfaces/product.interface';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const baseUrl = environment.baseUrl;
+
+interface ProductOptions {
+    limit?: number;
+    offset?: number;
+    gender: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class ProductsService {
+
+    private http = inject(HttpClient);
+
+    getProducts(options: ProductOptions):Observable<ProductsResponse> {
+
+        const {limit=9, offset=0, gender=''} = options;
+
+        const url = `${baseUrl}/products`;
+        return this.http.get<ProductsResponse>(url, {
+            params: {
+                limit: limit,
+                offset: offset,
+                gender: gender
+            }
+        })
+            .pipe(
+                tap( resp => console.log(resp))
+            );
+    }
+
+}
